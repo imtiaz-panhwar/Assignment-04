3b2d245b587907920e8be3354031b89973d5862d
imtiaz ali <adv.imtiazpanhwar@gmail.com>
2024-03-11 17:32:17 +0500

    assignment 04

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+//Qoute from famous person
+let quote = "A person who never made a mistake never tried any thing new";
+let author = "Albert Einstine";
+console.log(`${author} once said, "${quote}"`);
---
+++ app.ts
@@ -0,0 +1,7 @@
+//Qoute from famous person
+
+let quote:string = "A person who never made a mistake never tried any thing new";
+
+let author:string = "Albert Einstine";
+
+console.log(`${author} once said, "${quote}"`);
